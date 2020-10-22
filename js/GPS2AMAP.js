9ad0485e9989165071431ae5ac2f4c8b35bee742
R0000X <68675209+R0000X@users.noreply.github.com>
2020-10-22 20:17:56 +0800

    Create GPS2AMAP.js

---
+++ js/GPS2AMAP.js
@@ -0,0 +1,47 @@
+export const GPS = {
+  PI : 3.14159265358979324,
+  x_pi : 3.14159265358979324 * 3000.0 / 180.0,
+  delta : function (lat, lng) {
+      var a = 6378245.0; //  a: 卫星椭球坐标投影到平面地图坐标系的投影因子。
+      var ee = 0.00669342162296594323; //  ee: 椭球的偏心率。
+      var dLat = this.transformLat(lng - 105.0, lat - 35.0);
+      var dlng = this.transformlng(lng - 105.0, lat - 35.0);
+      var radLat = lat / 180.0 * this.PI;
+      var magic = Math.sin(radLat);
+      magic = 1 - ee * magic * magic;
+      var sqrtMagic = Math.sqrt(magic);
+      dLat = (dLat * 180.0) / ((a * (1 - ee)) / (magic * sqrtMagic) * this.PI);
+      dlng = (dlng * 180.0) / (a / sqrtMagic * Math.cos(radLat) * this.PI);
+      return {'lat': dLat, 'lng': dlng};
+  },
+
+  //GPS---高德
+  gcj_encrypt : function ( wgsLat , wgslng ) {
+      if (this.outOfChina(wgsLat, wgslng))
+          return {'lat': wgsLat, 'lng': wgslng};
+
+      var d = this.delta(wgsLat, wgslng);
+      return {'lat' : wgsLat + d.lat,'lng' : wgslng + d.lng};
+  },
+  outOfChina : function (lat, lng) {
+      if (lng < 72.004 || lng > 137.8347)
+          return true;
+      if (lat < 0.8293 || lat > 55.8271)
+          return true;
+      return false;
+  },
+  transformLat : function (x, y) {
+      var ret = -100.0 + 2.0 * x + 3.0 * y + 0.2 * y * y + 0.1 * x * y + 0.2 * Math.sqrt(Math.abs(x));
+      ret += (20.0 * Math.sin(6.0 * x * this.PI) + 20.0 * Math.sin(2.0 * x * this.PI)) * 2.0 / 3.0;
+      ret += (20.0 * Math.sin(y * this.PI) + 40.0 * Math.sin(y / 3.0 * this.PI)) * 2.0 / 3.0;
+      ret += (160.0 * Math.sin(y / 12.0 * this.PI) + 320 * Math.sin(y * this.PI / 30.0)) * 2.0 / 3.0;
+      return ret;
+  },
+  transformlng : function (x, y) {
+      var ret = 300.0 + x + 2.0 * y + 0.1 * x * x + 0.1 * x * y + 0.1 * Math.sqrt(Math.abs(x));
+      ret += (20.0 * Math.sin(6.0 * x * this.PI) + 20.0 * Math.sin(2.0 * x * this.PI)) * 2.0 / 3.0;
+      ret += (20.0 * Math.sin(x * this.PI) + 40.0 * Math.sin(x / 3.0 * this.PI)) * 2.0 / 3.0;
+      ret += (150.0 * Math.sin(x / 12.0 * this.PI) + 300.0 * Math.sin(x / 30.0 * this.PI)) * 2.0 / 3.0;
+      return ret;
+  }
+};
